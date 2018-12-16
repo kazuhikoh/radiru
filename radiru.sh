ad6d432563edda6a36cc3b58127ce7a6be205f66
kazuhikoh <kazuhiko.hrmt@gmail.com>
2018-12-16 19:44:33 +0900

    :sparkles: radiru info -m
    minimum output

--- radiru.sh
+++ radiru.sh
@@ -7,9 +7,12 @@ Usage: radiru <command> [argument...]
  Commands:
     info  shows archived program details. 
 
-Usage: radiru info <id>
+Usage: radiru info [-m] <id>
   \`id\` is query parameter `p` of url
   (http://www.nhk.or.jp/radio/ondemand/detail.html?p=4320_01 --> `4320_01`)
+
+  Options:
+  -m Minimum output.
 
 Examples:
   radiru info 4320_01 | jq '.main.detail_list[].file_list[] | {open_time, file_name}'
@@ -29,22 +32,62 @@ function ondemandUrl {
   echo $url
 }
 
-# info <id>
+# format_info_minimum_output <url> <date> <seq> <headline> <title>
+function format_info_minimum_output {
+  url=$1
+  date=$2
+  seq=$3
+  headline="$(
+    echo "$4" \
+      | sed 's; ;_;g' \
+      | sed 's;/;／;g' \
+      | sed 's;*;＊;g' \
+  )"
+  title="$(
+    echo "$5" \
+      | sed 's; ;_;g' \
+      | sed 's;/;／;g' \
+      | sed 's;*;＊;g' \
+  )"
+ 
+  echo $url $date $seq $headline $title
+}
+export -f format_info_minimum_output
+
+# info <id> [minimum]
 function info {
   local url=$(ondemandUrl $1)
-
-  curl "$url" 
+  local minimum=${2:-false}
+ 
+  if $minimum ; then
+    curl "$url" \
+      | jq -c '.main.detail_list[] | {headline, file_list:.file_list[] | {file_name, open_time, seq, file_title}}' \
+      | sed -E 's|^.*"headline":"(.+?)",.*"file_name":"(.+?)","open_time":"(....)\-(..)\-(..)[^ ]+".*"seq":(.+?),.*"file_title":"(.+?)".*}|\2 \3\4\5 \6 "'\''"\1"'\''" "'\''"\7"'\''"|g' \
+      | xargs -I{} bash -c "format_info_minimum_output {}"
+  else
+    curl "$url"
+  fi 
 }
 
 subcommand="$1"
 shift
+
+while getopts m opts
+do
+  case $opts in
+    m)
+      OUTPUT_MINIMUM=true
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
 
 case "$subcommand" in
   info)
     [ "$1" = "" ] && {
       exit 1
     }
-    info "$1"
+    info "$1" "$OUTPUT_MINIMUM"
     ;;
   *)
     usage
