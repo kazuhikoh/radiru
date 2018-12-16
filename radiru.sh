617d5e3072cc0e6804990cf0e329429b05462b25
kazuhikoh <kazuhiko.hrmt@gmail.com>
2018-12-16 13:56:59 +0900

    :sparkles: usage

--- radiru.sh
+++ radiru.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+function usage {
+  cat << EOF
+Usage: radiru <command> [argument...]
+
+ Commands:
+    info  shows on-demand program details. 
+
+Usage: radiru info <id>
+  \`id\` is query parameter `p` of url
+  (http://www.nhk.or.jp/radio/ondemand/detail.html?p=4320_01 --> `4320_01`)
+
+Examples:
+  radiru info 4320_01 | jq '.main.detail_list[].file_list[] | {open_time, file_name}'
+
+  radiru info 4320_01 | jq -c '.main.detail_list[].file_list[] | {file_name, open_time, seq, file_title}' | sed -E 's|^.*"file_name":"(.+?)","open_time":"(....)\-(..)\-(..)[^ ]+".*"seq":(.+?),.*"file_title":"(.+?)".*}|ffmpeg -i "\1" "pva.\2\3\4.\5.\6.mp3"|g'
+
+EOF
+}
+
 function ondemandUrl {
   local id="$1"
   local filepath="$2"
@@ -26,4 +45,8 @@ case "$subcommand" in
       exit 1
     }
     info "$1"
+    ;;
+  *)
+    usage
+    ;;
 esac
