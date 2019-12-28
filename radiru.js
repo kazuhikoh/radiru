11a2f7f1f656e495217e81f47a83f209285e40fa
kazuhikoh <kazuhiko.hrmt@gmail.com>
2019-12-29 01:23:45 +0900

    :sparkles: radiru download <site_id> <corner_id> <filename>

--- radiru.js
+++ radiru.js
@@ -3,6 +3,8 @@
 const app = require('commander');
 const api = require('./datasource/remote/radiru-api');
 
+const download = require('./internal/download');
+
 app
   .version('1.0.0');
 
@@ -26,6 +28,14 @@ app
       .subscribe(it => {
         console.log(JSON.stringify(it, null, '\t'));
       });
+  });
+
+app
+  .command('download <siteId> <cornerId> <filename>')
+  .option('-n, --noaction', 'no action. print filenames to be saved.')
+  .action((siteId, cornerId, filename, cmd) => {
+    const exec = (cmd.noaction ? download.execWithoutAction : download.exec);
+    exec(siteId, cornerId, filename).subscribe();
   });
 
 app.parse(process.argv);
